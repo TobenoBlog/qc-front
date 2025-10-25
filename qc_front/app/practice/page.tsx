a82146a687512e900733e299d8e8f6cefa20747a
TobenoBlog <orangepaiiii@gmail.com>
2025-10-25 22:39:33 +0900

    Update page.tsx

--- qc_front/app/practice/page.tsx
+++ qc_front/app/practice/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import dynamic from 'next/dynamic';
+console.log("BASE_URL:", process.env.NEXT_PUBLIC_API_BASE);
 
 // PracticeFlow コンポーネントをクライアント側で読み込む
 const Flow = dynamic(() => import('../../components/PracticeFlow'), { ssr: false });

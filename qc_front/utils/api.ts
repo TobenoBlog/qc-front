69eaeb7cdf8b433d84de1296fa271230e304b93f
TobenoBlog <orangepaiiii@gmail.com>
2025-10-25 22:28:13 +0900

    Update api.ts

--- qc_front/utils/api.ts
+++ qc_front/utils/api.ts
@@ -1,5 +1,5 @@
 export async function apiFetch<T>(path: string, token: string, init?: RequestInit): Promise<T> {
-  const base = process.env.NEXT_PUBLIC_QC_API_BASE!;
+  const base = process.env.NEXT_PUBLIC_API_BASE!;
   const res = await fetch(`${base}${path}`, {
     ...init,
     headers: {

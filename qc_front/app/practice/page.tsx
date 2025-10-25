8cb49bdf02e505879e636ac0c1f770ebbb58ca6e
TobenoBlog <orangepaiiii@gmail.com>
2025-10-26 00:22:11 +0900

    Update page.tsx

--- qc_front/app/practice/page.tsx
+++ qc_front/app/practice/page.tsx
@@ -1,10 +1,6 @@
-
 // app/practice/page.tsx
-import dynamic from "next/dynamic";
-
-// SSRなしで PracticeFlow を遅延読み込み
-const Flow = dynamic(() => import("@/components/PracticeFlow"), { ssr: false });
+import PracticeFlow from "@/components/PracticeFlow";
 
 export default function PracticePage() {
-  return <Flow />;
+  return <PracticeFlow />;
 }
